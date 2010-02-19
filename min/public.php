ace2ac2fc2ca3c45ac2ca4fb23659cb8e083fc8d
gerits aurelien <gerits.aurelien@gmail.com>
2010-02-19 13:13:25 +0000

    Suppression des fichiers thumb de windows Update de colorbox Modification dans les fichiers de minify (ajout de colorbox)
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@22 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- min/public.php
+++ min/public.php
@@ -4,10 +4,10 @@
  * minify dans public
  */
 return array(
-	'publiccss' => array('//framework/css/colorbox-black/colorbox.css','//framework/css/ui.selectmenu.css','//framework/css/ui/ui.checkbox.css'),
+	'publiccss' => array('//framework/css/colorbox/colorbox.css','//framework/css/ui.selectmenu.css','//framework/css/ui/ui.checkbox.css'),
 	'publicjs'=> array('//framework/js/jquery-1.4.1.min.js','//framework/js/jquery-ui-1.7.2.min.js',
 	'//framework/js/ui/i18n/jquery-ui-i18n.js','//framework/js/jquery.form-2.33.js','//framework/js/ui.selectmenu.js',
-	'//framework/js/jquery.validate.min.js','//framework/js/ui.checkbox.js','//framework/js/jquery.colorbox-min.js',
+	'//framework/js/jquery.validate.min.js','//framework/js/ui.checkbox.js','//framework/js/jquery.colorbox-min-1.6.js',
 	'//framework/js/jquery.cookie.js'),
 	'maxAge' => 31536000,
 	'setExpires' => time() + 86400 * 365
