6b5c2898b757b8fe11d536c5f880f01215d2adea
gerits aurelien <gerits.aurelien@gmail.com>
2010-03-18 09:40:50 +0000

    * Modification icône dans génération de sitemap * Correction de la prise en charge  de colorbox dans minify
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@59 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- min/public.php
+++ min/public.php
@@ -4,7 +4,7 @@
  * minify dans public
  */
 return array(
-	'publiccss' => array('//framework/css/colorbox/colorbox.css','//framework/css/ui.selectmenu.css','//framework/css/ui/ui.checkbox.css'),
+	'publiccss' => array('//framework/css/colorbox-white/colorbox.css','//framework/css/ui.selectmenu.css','//framework/css/ui/ui.checkbox.css'),
 	'publicjs'=> array('//framework/js/jquery-1.4.2.min.js','//framework/js/jquery-ui-1.7.2.min.js',
 	'//framework/js/ui/i18n/jquery-ui-i18n.js','//framework/js/jquery.form-2.40.js','//framework/js/ui.selectmenu.js',
 	'//framework/js/jquery.validate.min.js','//framework/js/ui.checkbox.js','//framework/js/jquery.colorbox-min-1.6.js',
