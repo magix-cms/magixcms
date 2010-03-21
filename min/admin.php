38d6ac683d30b4a6affcbe9e0964573ef79666e4
gerits aurelien <gerits.aurelien@gmail.com>
2010-03-21 22:20:10 +0000

    * Mise à jour de jquery form 2.40 => 2.43 * Modification dans la minification admin et public pour chargé jquery form 2.43
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@67 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- min/admin.php
+++ min/admin.php
@@ -8,7 +8,7 @@ return array(
 	'//framework/css/globalcss.css','//framework/css/colorbox-simple/colorbox.css','//framework/css/ui.selectmenu.css',
 	'//framework/css/globalforms.css','//framework/css/ui.spinner.css'),
 	'adminjs'=> array('//framework/js/jquery-1.4.2.min.js','//framework/js/jquery-ui-1.7.2.min.js',
-	'//framework/js/ui/i18n/jquery-ui-i18n.js','//framework/js/jquery.form-2.40.js','//framework/js/jquery.validate.min.js',
+	'//framework/js/ui/i18n/jquery-ui-i18n.js','//framework/js/jquery.form-2.43.js','//framework/js/jquery.validate.min.js',
 	'//framework/js/jquery.validate.password.js','//framework/js/jquery.cookie.js','//framework/js/jquery.colorbox-min-1.6.js',
 	'//framework/js/ui.checkbox.js','//framework/js/ui.spinner.js','//framework/js/ui.selectmenu.js',
 	'//framework/js/ad-globalform-1.0.js','//framework/js/ad-globaljs-1.0.js'),
