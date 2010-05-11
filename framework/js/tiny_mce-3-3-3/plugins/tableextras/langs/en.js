cdf67ad5f2e6e59c370c1b52807efa57970d0560
gerits aurelien <gerits.aurelien@gmail.com>
2010-05-11 11:33:50 +0000

    * Suppression de tinyMCE 3.3.3 pour procéder à la mise à jour
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@112 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/tiny_mce-3-3-3/plugins/tableextras/langs/en.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('en.tableextras',{
-	convertcellsinrow: 'Convert cell type for current row',
-	tabledraw : 'Create table',
-	tabledrawclose : 'Close'
-});
--- framework/js/tiny_mce-3-3-3/plugins/tableextras/langs/fr.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('fr.tableextras',{
-	convertcellsinrow: 'Convertir le type de cellule pour la ligne courante',
-	tabledraw : 'Créer un tableau',
-	tabledrawclose : 'Fermé'
-});
