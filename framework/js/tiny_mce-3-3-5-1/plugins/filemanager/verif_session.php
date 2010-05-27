2d2966b7ff51288ba0d09335d95bcf87fde25569
gerits aurelien <gerits.aurelien@gmail.com>
2010-05-27 13:30:19 +0000

    * Fix bug IE tinymce 3.3.5.1
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@146 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/tiny_mce-3-3-5-1/plugins/filemanager/verif_session.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if(isset($_SESSION['useradmin'])){
-	session_start();
-	$_SESSION['isLoggedIn'] = true;
-	header("location: " . $_REQUEST['return_url']);
-}
-?>
--- framework/js/tiny_mce-3-3-5-1/plugins/rj_insertcode/langs/ru.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('ru.rj_insertcode',{
-	desc : 'Вставить/редактировать код',
-	delta_width : 0,
-	delta_height : 0
-});
