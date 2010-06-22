6c87f40c69da6da7d59bea311e5094509715bef6
gerits aurelien <gerits.aurelien@gmail.com>
2010-06-22 07:46:47 +0000

    * Ajout du plugin contact dans le sitemap
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@187 9511e328-2ecd-428d-8f03-cf8620a26fe1

---
+++ plugins/contact/admin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @category   Plugins 
+ * @package    Magix CMS
+ * @copyright  Copyright (c) 2010 - 2011 (http://www.logiciel-referencement-professionnel.com/)
+ * @license    Proprietary software
+ * @version    1.0 03-06-2010
+ * Update      1.1 11-06-2010
+ * @author Gérits Aurélien <aurelien@web-solution-way.be>
+ * @name contact
+ * @version 0.2
+ *
+ */
+class plugins_contact_admin{
+/**
+	 * Fonction pour la création des urls dans le sitemap
+	 * !!! createSitemap obligatoire pour l'ajout dans le sitemap
+	 */
+	public function createSitemap(){
+		/*instance la classe*/
+        $sitemap = new magixcjquery_xml_sitemap();
+		   $sitemap->writeMakeNode(
+				'magixmod/contact/',
+				date('d-m-Y'),
+				'always',
+				0.7
+		   );
+	}
+}
