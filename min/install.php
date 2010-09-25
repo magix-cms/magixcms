21ef6e3d693588995e2077ca58391193f78ac0af
gerits aurelien <gerits.aurelien@gmail.com>
2010-09-25 19:31:09 +0000

    * Upgrade jquery UI 1.8.4 => 1.8.5 + Upgrade thème dark backend 1.8.5 	* Mise à jour tinyMCE 3.3.8 => 3.3.9 	* Upgrade magixcjquery 3.0 => 3.1 	* Ajout des licences Open source GPL3 + MIT
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@214 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- min/install.php
+++ min/install.php
@@ -4,11 +4,11 @@
  * minify install
  */
 return array(
-	'installcss' => array('//framework/css/ui/dark-backend-1-8-4/jquery-ui-1.8.4.custom.css','//framework/css/ui/ui.checkbox.1.4.css',
+	'installcss' => array('//framework/css/ui/dark-backend-1-8-5/jquery-ui-1.8.5.custom.css','//framework/css/ui/ui.checkbox.1.4.css',
 	'//framework/css/globalcss.css','//framework/css/colorbox-simple/colorbox.css','//framework/css/ui/ui.selectmenu.1.8.4.css',
 	'//framework/css/globalforms.css','//install/css/nivo-slider.css','//framework/css/notification.css','//install/css/install.css'),
-	'installjs'=> array('//framework/js/jquery-1.4.2.min.js','//framework/js/jquery-ui-1.8.4.custom.min.js',
-	'//framework/js/ui/i18n-1.8/jquery-ui-i18n.js','//framework/js/jquery.form-2.43.js','//framework/js/ui/ui.selectmenu-1-8-4.js',
+	'installjs'=> array('//framework/js/jquery-1.4.2.min.js','//framework/js/jquery-ui-1.8.5.custom.min.js',
+	'//framework/js/ui/i18n-1.8/jquery-ui-i18n.js','//framework/js/jquery.form-2.47.js','//framework/js/ui/ui.selectmenu-1-8-4.js',
 	'//framework/js/jquery.validate-1.7.js','//framework/js/jquery.validate.password-1.0.js','//framework/js/ui/ui.checkbox.1.4.js','//framework/js/jquery.colorbox-min-1.3.9.js',
 	'//framework/js/jquery.cookie.js','//framework/js/ad-globalform-1.0.js','//install/js/jquery.nivo.slider.pack.js','//framework/js/jquery.meerkat.1.3.js','//framework/js/jquery.jfirebug.js','//install/js/install.js'),
 	'maxAge' => 31536000,
