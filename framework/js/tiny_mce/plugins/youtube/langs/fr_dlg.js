4cd0d5f6923a8325ed3d8fec9935f19d1c8f61ba
gerits aurelien <gerits.aurelien@gmail.com>
2010-12-12 03:34:07 +0000

    * Ajout du plugin youtube dans tinyMCE * Ajout de la classe pour la gestion des entête HTTP dans magixglobal * Modification des entêtes json dans le catalogue, CMS avec la nouvelle classe header * Fix bug de la mise en cache des fichiers json et php via le htaccess
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@268 9511e328-2ecd-428d-8f03-cf8620a26fe1

---
+++ framework/js/tiny_mce/plugins/youtube/langs/fr_dlg.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('fr.youtube_dlg',{
+    title : "Video YouTube",
+    instr : "Insérez l'identifiant YouTube",
+    ytID : "ID",
+    ytW : "Width",
+    ytH : "Height",
+    relvideo: "Inclure les vidéos similaires",
+    hd:"Visionner en HD",
+    yes:"Oui",
+    no:"Non"
+});
