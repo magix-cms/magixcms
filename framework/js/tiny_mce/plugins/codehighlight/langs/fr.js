3b7f60b2ef6fca8fb7bdb13f13e70ab6943ab096
gerits aurelien <gerits.aurelien@gmail.com>
2011-03-25 08:01:01 +0000

    Suppression de tinymce 3.4
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@334 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/tiny_mce/plugins/codehighlight/langs/fr.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('fr.codehighlight',{
-	desc : 'Insérer un code utilisant Syntaxhighlighter 3'
-});
--- framework/js/tiny_mce/plugins/syntaxhl/langs/fr.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('fr.syntaxhl',{
-	desc : 'Insert code using Syntaxhighlighter'
-});
