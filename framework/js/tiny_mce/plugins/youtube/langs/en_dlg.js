3cc48da346dc900428d69de974d7a97ed00536b3
gerits aurelien <gerits.aurelien@gmail.com>
2011-07-07 10:24:01 +0000

    * Ajout de tinymce 3.4.3.2
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@382 9511e328-2ecd-428d-8f03-cf8620a26fe1

---
+++ framework/js/tiny_mce/plugins/youtube/langs/en_dlg.js
@@ -0,0 +1,12 @@
+tinyMCE.addI18n('en.youtube_dlg',{
+    title : "Insert a YouTube Video",
+    instr : "Link format for sharing : http://youtu.be/xxxxxxxx",
+    ytID : "Link Sharing",
+    ytW : "Width",
+    ytH : "Height",
+    autoplay: "Autoplay",
+    relvideo: "Related videos",
+    hd:"Watch in HD",
+    yes:"Yes",
+    no:"No"
+});
