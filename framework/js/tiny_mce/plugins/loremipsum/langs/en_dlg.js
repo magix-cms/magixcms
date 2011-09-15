ea361861b5708698f0a79df43ab13b01761380a6
gerits aurelien <gerits.aurelien@gmail.com>
2011-09-15 08:07:07 +0000

    * Update tinymce 3.4.5
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@455 9511e328-2ecd-428d-8f03-cf8620a26fe1

---
+++ framework/js/tiny_mce/plugins/loremipsum/langs/en_dlg.js
@@ -0,0 +1,20 @@
+/**
+ * Lorem Ipsum plug-in for TinyMCE version 3.x
+ * -------------------------------------------
+ * $Id: en_dlg.js 6 2008-06-18 08:56:05Z scholzj $
+ *
+ * @author     JAkub Scholz
+ * @version    $Rev: 6 $
+ * @package    LoremIpsum
+ * @link       http://www.assembla.com/spaces/lorem-ipsum
+ */
+
+tinyMCE.addI18n('en.loremipsum_dlg',{
+	sentences : "Source text",
+	amount : "Generated amount",
+	formating : "Formating",
+	unformated : "Unformated sentences",
+	paragraphs : "Paragraphs &lt;p&gt;",
+	list : "List &lt;ul&gt;",
+	orderedlist : "Ordered List &lt;ol&gt;"
+});
---
+++ framework/js/tiny_mce/plugins/productsearch/js/dialog.js
@@ -0,0 +1,86 @@
+tinyMCEPopup.requireLangPack();
+function insert_product_catalog_link(href,name){
+	tinyMCE.execCommand('mceInsertContent',false,'<a title="'+name+'" href="'+href+'">'+name+'</a>');
+}
+var ProductSearchDialog = {
+	init : function() {
+		var t = this;
+		t._search();
+	},
+	_search : function(){
+		var t = this;
+		$("#forms-search-product").submit(function(){
+			$(this).ajaxSubmit({
+	    		url: '/admin/catalog.php?get_search_product=true',
+	    		type:"post",
+	    		dataType:"json",
+	    		resetForm: true,
+	    		beforeSubmit:function(){
+	    			$('#table_search_product tbody').empty();
+	    			$('#table_search_product tbody').html('<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td><img src="/framework/img/small_loading.gif" /></td><td>&nbsp;</td><td>&nbsp;</td></tr>');
+	    		},
+	    		success:function(request) {
+	    			t._result(request);
+	    		}
+	    	});
+			return false; 
+		});
+	},
+	_addslashes: function(ch) {
+		ch = ch.replace(/\\/g,"\\\\");
+		ch = ch.replace(/\'/g,"\\'");
+		ch = ch.replace(/\"/g,"\\\"");
+		return ch;
+	},
+	_result :function(j){
+		var t = this;
+		$('#table_search_product tbody').empty();
+		if(j === undefined){
+			console.log(j);
+		}
+		if(j !== null){
+			$.each(j, function(i,item) {
+				if(item.iso != null){
+					flaglang = item.iso;
+				}else{
+					flaglang = '-';
+				}
+				if(item.iso != null){
+					flaglang = item.iso;
+				}else{
+					flaglang = '-';
+				}
+				if(item.subcategory != null){
+					scategory = item.subcategory;
+				}else{
+					scategory = '-';
+				}
+				var titleproduct = t._addslashes(item.titlecatalog);
+				//tinyMCEPopup.editor
+				//tinyMCEPopup.restoreSelection();
+				$('<tr>'
+				+'<td>'+item.idproduct+'</td>'
+				+'<td>'+flaglang+'</td>'
+				+'<td>'+item.titlecatalog+'</td>'
+				+'<td>'+item.category+'</td>'
+				+'<td>'+scategory+'</td>'
+				+'<td><a href="javascript:;" onclick="tinyMCEPopup.close();" onmousedown="insert_product_catalog_link(\''+item.uriproduct+'\',\''+titleproduct+'\');">Insert</a></td>'
+				+'</tr>').appendTo('#table_search_product tbody');
+			});
+		}else{
+			$('<tr>'
+				+'<td>-</td>'
+				+'<td>-</td>'
+				+'<td>-</td>'
+				+'<td>-</td>'
+				+'<td>-</td>'
+				+'<td>-</td>'
+				+'</tr>').appendTo('#table_search_product tbody');
+		}
+	},
+	insert: function () {
+		return null;
+	}
+};
+
+tinyMCEPopup.onInit.add(ProductSearchDialog.init, ProductSearchDialog);
