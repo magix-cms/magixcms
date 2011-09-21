fe58a808efd99b8fbf240feb6b92b7d44f10e65e
gerits aurelien <gerits.aurelien@gmail.com>
2011-09-21 11:44:05 +0000

    * Fix bug pdw configuration (tinyMCE conf)
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@460 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/backend/tinymce-config.js
+++ framework/js/backend/tinymce-config.js
@@ -29,7 +29,7 @@ $(function() {
 			theme_advanced_buttons2 : "image"+manager_tinymce_button+filemanager_tinymce_button+",|,loremipsum,youtube,|,cmspage,news_search"+catalog_tinymce_button+",|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,forecolor,charmap,|,link,unlink,anchor",
 			theme_advanced_buttons3 : "tablegrid,|,row_props,cell_props,|,row_before,row_after,delete_row,|,col_before,col_after,delete_col,|,split_cells,merge_cells,|,backcolor,hr,removeformat,|,code,preview,fullscreen",
 			theme_advanced_buttons4 : "codehighlight,media,insertdate,inserttime,|,cut,copy,paste,pastetext,pasteword,",
-			pdw_toggle_on : "1,2",
+			pdw_toggle_on : 1,
 	        pdw_toggle_toolbars : "3,4",
 			/*tabledraw,convertcelltype,*/
 			//tabledraw,convertcelltype,|
