7f228f5982b14d631600c5d4489e0d13d0aed203
gerits aurelien <gerits.aurelien@gmail.com>
2011-12-04 20:47:54 +0000

    * Ajout de la localization dans jquery validate * Modification du formulaire de contact pour le chargement de l'isovalidate
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@493 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- plugins/contact/js/public.0.2.js
+++ plugins/contact/js/public.0.2.js
@@ -30,35 +30,7 @@ var plugins_contact = {
 					minlength: 2
 				}
 			},
-			messages: {
-				nom: {
-					required: '* Enter a lastname'
-				},
-				prenom: {
-					required: '* Enter a firstname'
-				},
-				email: {
-					required: "* Enter a email",
-					email: "* Enter a valid mail: johndoe@mail.com"
-				},
-				message: {
-					required: "* Enter a message"
-				}
-			},
 			submitHandler: function(form) {
-			 	/*$(form).ajaxSubmit({
-				 	url:'/'+lang+'/magixmod/contact/',
-				 	type:"post",
-				 	resetForm: true,
-				 	beforeSubmit:function(){},
-				 	success:function(request){
-				 		$.notice({
-							ntype: "simple",
-							time:2
-						});
-        				$(".mc-head-request").html(request);
-				 	}
-				});*/
 				$.nicenotify({
 					ntype: "submit",
 					uri: '/'+lang+'/magixmod/contact/',
