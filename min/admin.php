60cb640bbc8880066878eae47879abf2151b58de
gerits aurelien <gerits.aurelien@gmail.com>
2011-12-11 20:35:46 +0000

    * Update jquery form 2.87 => 2.93      - Modification des fichiers dépendant de jquery form
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@501 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- min/admin.php
+++ min/admin.php
@@ -38,7 +38,7 @@ return array(
 	'//framework/css/colorbox-simple/colorbox.css',
 	'//framework/css/ui/ui.spinner.1-20.css','//framework/css/notification.css','//framework/css/jquery.jqplot.css','//framework/css/globalforms.css','//framework/css/globalcss.css'),
 	'globaljs'=> array('//framework/js/jquery-1.7.1.min.js','//framework/js/jquery-ui-1.8.15.custom.min.js','//framework/js/ui/i18n-1.8.15/jquery-ui-i18n.js',
-	'//framework/js/jquery.form.2.87.js','//framework/js/jquery.validate.1.9.min.js','//framework/js/additional-methods.1.9.min.js',
+	'//framework/js/jquery.form.2.93.js','//framework/js/jquery.validate.1.9.min.js','//framework/js/additional-methods.1.9.min.js',
 	'//framework/js/jquery.validate.password-1.0.js','//framework/js/jquery.cookie.js','//framework/js/tools/jquery.colorbox-1.3.18.js',
 	'//framework/js/ui/ui.spinner.1-20.min.js','//framework/js/jquery.jfirebug.js','//framework/js/tools/notice-tpl.js',
 	'//framework/js/backend/magixtools.0.1.js','//framework/js/jimagine/plugins/jquery.nicenotify.js','//framework/js/jimagine/config.js','//framework/js/jimagine/constant.js'),
--- min/public.php
+++ min/public.php
@@ -36,7 +36,7 @@
 return array(
 	'publiccss' => array('//framework/css/notification.css'),
 	'publicjs'=> array('//framework/js/jquery-1.7.1.min.js','//framework/js/jquery-ui-1.8.15.custom.min.js',
-	'//framework/js/ui/i18n-1.8.15/jquery-ui-i18n.js','//framework/js/jquery.form.2.87.js',
+	'//framework/js/ui/i18n-1.8.15/jquery-ui-i18n.js','//framework/js/jquery.form.2.93.js',
 	'//framework/js/jquery.validate.1.9.min.js','//framework/js/additional-methods.1.9.min.js','//framework/js/tools/jquery.colorbox-1.3.18.js',
 	'//framework/js/jquery.cookie.js'),
 	'jimagine' => array('//framework/js/jimagine/plugins/jquery.nicenotify.js','//framework/js/jimagine/plugins/jquery.fbwidget.js',
