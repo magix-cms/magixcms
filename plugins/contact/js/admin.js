e004edf87abc244ba3b827eb2bcd41ceb0c8f51a
gerits aurelien <gerits.aurelien@gmail.com>
2012-02-09 23:26:01 +0000

    * Préparation de l'upgrade du plugin contact (optimisation)
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@521 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- plugins/contact/js/admin.js
+++ plugins/contact/js/admin.js
@@ -1,9 +1,9 @@
 /**
  * MAGIX CMS
- * @copyright  MAGIX CMS Copyright (c) 2011 Gerits Aurelien, 
+ * @copyright  MAGIX CMS Copyright (c) 2012 Gerits Aurelien, 
  * http://www.magix-cms.com, http://www.magix-cjquery.com
  * @license    Dual licensed under the MIT or GPL Version 3 licenses.
- * @version    1.0
+ * @version    1.1
  * @author Gérits Aurélien <aurelien@magix-cms.com>
  * @name adminjs
  * @package plugins
@@ -14,26 +14,36 @@ $(function() {
 	 /**
      * Requête ajax pour l'ajout des contacts
      */
-	 $("#forms-plugins-contact").submit(function(){
-		$.nicenotify({
-			ntype: "submit",
-			uri: '/admin/plugins.php?name=contact&add',
-			typesend: 'post',
-			idforms: $(this),
-			beforeParams:function(){},
-			successParams:function(e){
-				$.nicenotify.initbox(e,{
-					//display:false,
-					reloadhtml:true
-				});
+	var formsAddContact = $("#forms-plugins-contact").validate({
+		onsubmit: true,
+		event: 'submit',
+		rules: {
+			idadmin: {
+				required: true
 			}
-		});
-		return false; 
+		},
+		submitHandler: function(form) {
+			$.nicenotify({
+				ntype: "submit",
+				uri: '/admin/plugins.php?name=contact&add',
+				typesend: 'post',
+				idforms: $(form),
+				beforeParams:function(){},
+				successParams:function(e){
+					$.nicenotify.initbox(e,{
+						//display:false,
+						reloadhtml:true
+					});
+				}
+			});
+			return false; 
+		}
 	});
+	$("#forms-plugins-contact").formsAddContact;
 	 /**
      * Requête ajax pour la suppression des contacts
      */
-	$('.d-plugins-contact').click(function (event){
+	 $(document).on('click','.d-plugins-contact',function (event){
 		event.preventDefault();
 		var lg = $(this).attr("title");
 		$("#dialog").dialog({
