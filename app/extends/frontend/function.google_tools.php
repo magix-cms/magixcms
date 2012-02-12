c0ae2d62074451e18b32d095845405f3f13b3626
gerits aurelien <gerits.aurelien@gmail.com>
2012-02-12 14:29:22 +0000

    * Fix bug google tools (modification du code analytics)
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@530 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- app/extends/frontend/function.google_tools.php
+++ app/extends/frontend/function.google_tools.php
@@ -59,17 +59,20 @@ function smarty_function_google_tools($params, $template){
 		$analyticsdata = frontend_model_setting::select_uniq_setting('analytics');
 		$analytics = $analyticsdata['setting_value'];
 		if($analytics != null){
-		$tools = <<<EOT
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', '$analytics']);
-		  _gaq.push(['_trackPageview']);
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
+$tools = <<<EOT
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$analytics']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 EOT;
 		}else{
 			$tools = '';
