b36a2890a5cec4431ec87b23e9e4400b04befcd0
gerits aurelien <gerits.aurelien@gmail.com>
2012-02-26 20:45:35 +0000

    * Suppression de jquery tinycme 3.4.8
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@533 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/tiny_mce/plugins/news_search/langs/en_dlg.js
+++
@@ -1,9 +0,0 @@
-tinyMCE.addI18n('en.news_search_dlg',{
-	title : 'Search News',
-	description: 'Search for a News to insert a link',
-	search: 'Search',
-	lang: 'Lang',
-	date_register: 'Date',
-	subject: 'Subject',
-	link: 'Link'
-});
