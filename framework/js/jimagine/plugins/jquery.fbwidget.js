0984e270683b19c58d1f7b8101c692412c44301d
gerits aurelien <gerits.aurelien@gmail.com>
2012-05-27 23:00:19 +0000

    * Upgrade du plugin jquery fbwidget
    git-svn-id: http://magixdev.indefero.net/svn/magixdev/magixcms/trunk@544 9511e328-2ecd-428d-8f03-cf8620a26fe1

--- framework/js/jimagine/plugins/jquery.fbwidget.js
+++ framework/js/jimagine/plugins/jquery.fbwidget.js
@@ -1,81 +1,93 @@
 /*
-# -- BEGIN LICENSE BLOCK ----------------------------------
-#
-# This file is part of Jimagine.
-# Toolbox for jQuery
-# Copyright (C) 2011 - 2012  Gerits Aurelien <aurelien[at]magix-dev[dot]be>
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+ # -- BEGIN LICENSE BLOCK ----------------------------------
+ #
+ # This file is part of Jimagine.
+ # Toolbox for jQuery
+ # Copyright (C) 2011 - 2012  Gerits Aurelien <aurelien[at]magix-dev[dot]be>
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# -- END LICENSE BLOCK -----------------------------------
-*/
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ #
+ # -- END LICENSE BLOCK -----------------------------------
+ */
 /**
  * MAGIX DEV
- * @copyright  MAGIX DEV Copyright (c) 2011 - 2012 Gerits Aurelien, 
+ * @copyright  MAGIX DEV Copyright (c) 2011 - 2012 Gerits Aurelien,
  * http://www.magix-dev.be
  * @license    Dual licensed under the MIT or GPL Version 3 licenses.
  * @version    0.1
  * @author Gérits Aurélien <aurelien[at]magix-dev[dot]be>
  * @name socialXFBML
- * @link
+ *
  * Exemple for French : http://connect.facebook.net/fr_FR/all.js#xfbml=1
  * @exemple with like box facebook widget:
  * $("#fb_like_box").fbwidget({
-		widget : "like-box", 
-		attributs : {
-			"width":"440",
-			"show_faces":true,
-			"stream":false,
-			"header":false,
-			"border_color":"",
-			"href":"https://www.facebook.com/pages/Magix-Dev/227636040597626"
-		}
-	});
+ widget : "like-box",
+ attributs : {
+ "width":"440",
+ "show_faces":true,
+ "stream":false,
+ "header":false,
+ "border_color":"",
+ "href":"https://www.facebook.com/pages/Magix-Dev/227636040597626"
+ }
+ });
+ * $("#fb_like").fbwidget({
+ widget : "like",
+ attributs : {
+ "width":"100",
+ "show_faces":false,
+ "stream":false,
+ "header":false,
+ "border_color":"",
+ "layout":"button_count",
+ "href":"https://www.facebook.com/pages/Magix-Dev/227636040597626"
+ }
+ * });
  */
 (function($){
-	$.fn.fbwidget = function(settings){
-		// Default options value
-	    var options = {
-	      widget: "like",
-	      attributs: {}
-	    };
-	    if ($.isPlainObject(settings)) {
-	    	var o = $.extend(true,options, settings || {});
+    $.fn.fbwidget = function(settings){
+        // Default options value
+        var options = {
+            widget: "like",
+            attributs: {}
+        };
+        if ($.isPlainObject(settings)) {
+            var o = $.extend(true,options, settings || {});
         }else{
-        	console.log("%s: %o","fbwidget settings is not object");
+            console.log("%s: %o","fbwidget settings is not object");
         }
-	    //var o = options;
-	    // Reference to the container element
-    	var obj = $(this);
-		//params = params || {};
-	    if(o.widget != ""){
-	    	return this.each(function() {
-		    	fb_dom = $("<fb:" + o.widget + "></fb:" + o.widget + ">");
-		    	if(options.attributs != ''){
-		    		$.each(options.attributs, function( key , value ) {
-		    			fb_dom.attr( key , value );
-		    		});
-		    		fb_dom.appendTo(obj);
-		    	}else{
-		    		console.log("%s: %o","attributs is null",o.widget);
-		    	}
-	    	});
+        //var o = options;
+        // Reference to the container element
+        var obj = $(this);
+        //params = params || {};
+        if(o.widget != ""){
+            return this.each(function() {
+                fb_dom = $("<fb:" + o.widget + "></fb:" + o.widget + ">");
+                if(options.attributs != ''){
+                    $.each(options.attributs, function( key , value ) {
+                        fb_dom.attr( key , value );
+                    });
+                    fb_dom.appendTo(obj);
+                }else{
+                    console.log("%s: %o","attributs is null",o.widget);
+                }
+            });
         }else if(o.widget == undefined){
-        	console.log("%s: %o","widget is undefined",o.widget);
-        	return false;
+            console.log("%s: %o","widget is undefined",o.widget);
+            return false;
         }else{
-        	console.log("%s: %o","widget is not found",o.widget);
-        	return false;
+            console.log("%s: %o","widget is not found",o.widget);
+            return false;
         }
-	};
+    };
 })(jQuery);
