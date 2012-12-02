7812b1d80866103d31421cac240d5c57658d2ea9
gerits aurelien <gerits.aurelien@gmail.com>
2012-12-02 23:30:12 +0100

    * Organisation du modele dashboard
    * Suppression des icônes de boostrap dans la feuille principal (prise
    en charge "font")
    * Organisation des sections
    * Modification de l'entête HTML

--- admin/min/groupsConfig.php
+++ admin/min/groupsConfig.php
@@ -21,8 +21,8 @@ if(file_exists($configdir)){
 return array(
 	'adminjs' => array('//framework/library/jquery-1.8.3.min.js','//framework/library/jquery-ui-1.9.2.custom.min.js',
         '//framework/plugins/jquery.form.3.20.js','//framework/plugins/jquery.validate.1.10.0.min.js'),
-	'jimagine' => array('//framework/library/jimagine/lib/config.js','//framework/library/jimagine/lib/jmConstant.js',
-	'//framework/library/jimagine/lib/plugins/jquery.nicenotify.js','///framework/library/jimagine/lib/plugins/jquery.jmShowIt.js'
+	'jimagine' => array('//framework/library/jimagine/config.js','//framework/library/jimagine/jmConstant.js',
+	'//framework/library/jimagine/plugins/jquery.nicenotify.js','///framework/library/jimagine/plugins/jquery.jmShowIt.js'
 	),
 	'globalize'=> array('//framework/library/globalize/globalize.js','//framework/library/globalize/cultures/globalize.cultures.js'),
     'css' => array('//'.PATHADMIN.'/template/css/bootstrap.2.2.1.css','//'.PATHADMIN.'/template/css/bootstrap-responsive.2.2.1.css','//'.PATHADMIN.'/template/css/font-awesome.css','//'.PATHADMIN.'/template/css/style.css')
---
+++ admin/template/js/mc_dashboard.js
@@ -0,0 +1,72 @@
+/*
+ # -- BEGIN LICENSE BLOCK ----------------------------------
+ #
+ # This file is part of MAGIX CMS.
+ # MAGIX CMS, The content management system optimized for users
+ # Copyright (C) 2008 - 2012 sc-box.com <support@magix-cms.com>
+ #
+ # OFFICIAL TEAM :
+ #
+ #   * Gerits Aurelien (Author - Developer) <aurelien@magix-cms.com>
+ #
+ # Redistributions of files must retain the above copyright notice.
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ #
+ # -- END LICENSE BLOCK -----------------------------------
+
+ # DISCLAIMER
+
+ # Do not edit or add to this file if you wish to upgrade MAGIX CMS to newer
+ # versions in the future. If you wish to customize MAGIX CMS for your
+ # needs please refer to http://www.magix-cms.com for more information.
+ */
+/**
+ * Author: Gerits Aurelien <aurelien[at]magix-cms[point]com>
+ * Copyright: MAGIX CMS
+ * Date: 2/12/12
+ * Time: 21:27
+ * License: Dual licensed under the MIT or GPL Version
+ */
+var MC_dashboard = (function ($, undefined) {
+    //Fonction Private
+    function loadVersion(){
+        $.nicenotify({
+            ntype: "ajax",
+            uri: '/admin/version.php',
+            typesend: 'get',
+            beforeParams:function(){
+                var loader = $(document.createElement("span")).addClass("min-loader").append(
+                    $(document.createElement("img"))
+                        .attr('src','/framework/img/small_loading.gif')
+                        .attr('width','20px')
+                        .attr('height','20px')
+                )
+                $('#version').html(loader);
+            },
+            successParams:function(e){
+                $('.min-loader').remove();
+                $('#version').html(e);
+                $.nicenotify.initbox(e,{
+                    display:false
+                });
+            }
+        });
+    }
+    return {
+        //Fonction Public        
+        run:function () {
+            loadVersion();
+        }
+    };
+})(jQuery);
