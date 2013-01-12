5c353f6f6d54d2d19909fe6613736e2a2a6e7898
gerits aurelien <gerits.aurelien@gmail.com>
2013-01-12 02:53:12 +0100

    * Déplacement des fichiers JS utilisés uniquement dans le backend

--- admin/min/groupsConfig.php
+++ admin/min/groupsConfig.php
@@ -13,15 +13,16 @@
 
 return array(
 	'adminjs' => array('//framework/library/jquery-1.8.3.min.js','//framework/library/jquery-ui-1.9.2.custom.min.js',
-     '//framework/plugins/jquery.form.3.20.js','//framework/plugins/jquery.validate.1.10.0.min.js','//framework/library/bootstrap.min.js'),
+     '//framework/plugins/jquery.form.3.20.js','//framework/plugins/jquery.validate.1.10.0.min.js','//'.PATHADMIN.'/template/js/plugins/bootstrap.min.js'),
 	'jimagine' => array('//framework/library/jimagine/config.js','//framework/library/jimagine/jmConstant.js',
 	'//framework/library/jimagine/plugins/jquery.nicenotify.js','//framework/library/jimagine/plugins/jquery.jmShowIt.js',
-    '//framework/plugins/jquery.fancybox.min.js','//'.PATHADMIN.'/template/js/setting.js'),
+    '//'.PATHADMIN.'/template/js/plugins/jquery.fancybox.min.js','//'.PATHADMIN.'/template/js/setting.js'),
 	'globalize'=> array('//framework/library/globalize/globalize.js','//framework/library/globalize/cultures/globalize.cultures.js'),
-    'charts'=>array('//framework/library/raphael-min.js','//framework/library/morris.min.js'),
+    'charts'=>array('//'.PATHADMIN.'/template/js/plugins/raphael-min.js','//'.PATHADMIN.'/template/js/plugins/morris.min.js'),
     'tinymce' => array('//'.PATHADMIN.'/template/js/tiny_mce.'.VERSION_EDITOR.'/jquery.tinymce.js'),
     'css' => array('//'.PATHADMIN.'/template/css/bootstrap.2.2.2.css','//'.PATHADMIN.'/template/css/font-awesome.css',
-    '//'.PATHADMIN.'/template/css/ui-bootstrap/jquery-ui-1.9.2.custom.css','//'.PATHADMIN.'/template/css/fancybox/jquery.fancybox.css','//'.PATHADMIN.'/template/css/style.css'),
+    '//'.PATHADMIN.'/template/css/ui-bootstrap/jquery-ui-1.9.2.custom.css','//'.PATHADMIN.'/template/css/fancybox/jquery.fancybox.css',
+    '//'.PATHADMIN.'/template/css/style.css'),
     'maxAge' => 31536000,
     'setExpires' => time() + 86400 * 365
 );
