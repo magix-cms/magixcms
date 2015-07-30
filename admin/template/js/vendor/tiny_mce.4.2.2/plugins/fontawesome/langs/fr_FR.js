ff597a8e8c4b889f4b72acca503a68da4b27d369
gtraxx <gerits.aurelien@gmail.com>
2015-07-30 09:43:47 +0200

    Add Font Awesome plugin in tinymce

--- admin/template/js/vendor/tiny_mce.4.2.2/plugins/fontawesome/langs/fr_FR.js
+++ admin/template/js/vendor/tiny_mce.4.2.2/plugins/fontawesome/langs/fr_FR.js
@@ -1,17 +1,4 @@
-tinymce.addI18n('en', {
-    'Icons': 'Icons',
-    'Web Application': 'Web Application',
-    'File Type': 'File Type',
-    'Spinner': 'Spinner',
-    'Form Control': 'Form Control',
-    'Currency': 'Currency',
-    'Text Editor': 'Text Editor',
-    'Directional': 'Directional',
-    'Video Player': 'Video Player',
-    'Brand': 'Brand',
-    'Medical': 'Medical',
-    'Transportation': 'Transportation',
-    'Gender': 'Gender',
-    'Payment': 'Payment',
-    'Chart': 'Chart'
+tinymce.addI18n('fr_FR', {
+    'Icons': 'Icônes',
+    'Icons List': "Liste d'icônes"
 });
