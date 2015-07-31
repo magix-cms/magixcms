969f1d337c81e2334d2f055a8725a0a649991254
gtraxx <gerits.aurelien@gmail.com>
2015-07-31 10:58:45 +0200

    Update tinymce 4.2.2 => 4.2.3

--- admin/template/js/vendor/tiny_mce.4.2.2/plugins/mc_news/langs/fr_FR.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('fr_FR',{
-	'mc_news Title' : "Rechercher une News",
-	'search' : 'Rechercher',
-    'date_register': 'Date',
-    'title' : 'Titre',
-    'mc_news description' : 'Rechercher une actualité pour insérer un lien',
-    'link' : 'Liens'
-});
--- admin/template/js/vendor/tiny_mce.4.2.2/plugins/mc_pages/langs/fr_FR.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('fr_FR',{
-	'mc_pages Title' : "Rechercher une page",
-	'search' : 'Rechercher',
-	'category' : 'Catégorie',
-    'title' : 'Titre',
-    'mc_pages description' : 'Rechercher une page CMS pour insérer un lien',
-    'link' : 'Liens'
-});
