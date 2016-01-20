c2c9f9b0a15ff92c36b718d3d8c1afa8135925b3
gtraxx <gerits.aurelien@gmail.com>
2016-01-20 14:22:08 +0100

    Remove advantage widget in default (frontend)

--- skin/default/widget/function.widget_advantage_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-function smarty_function_widget_advantage_data($params, $template){
-    plugins_Autoloader::register();
-    $collection = new plugins_advantage_public();
-
-    $template->assign('advantages',$collection->getAdvs());;
-}
-?>
