e4c735494381e74228a8727e1a4d07f7563ef1f4
gtraxx <gerits.aurelien@gmail.com>
2016-02-23 14:39:47 +0100

    Fix getPosition in global js

--- skin/default/js/src/global.js
+++ skin/default/js/src/global.js
@@ -29,6 +29,18 @@ function initGallery(titles,iso){
 	});
 }
 
+function getPosition(element) {
+	var xPosition = 0;
+	var yPosition = 0;
+
+	while(element) {
+		xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+		yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+		element = element.offsetParent;
+	}
+	return { x: xPosition, y: yPosition };
+}
+
 $(function()
 {
 	// *** In case you don't have firebug...
