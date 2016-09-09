2e41de495de4c98852a396ff6164c639a8d888c5
gtraxx <gerits.aurelien@gmail.com>
2016-09-09 10:49:06 +0200

    Fix update column name country order

--- install/sql/update.2.7.0.sql
+++ install/sql/update.2.7.0.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `mc_country` (
   `idcountry` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `iso` varchar(5) NOT NULL,
   `country` varchar(125) NOT NULL,
-  `order` int(5) unsigned NOT NULL DEFAULT '0',
+  `order_c` int(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`idcountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
