ea8d2df7e85e7cd747c3893a4ec1ccb685372a31
gtraxx <gerits.aurelien@gmail.com>
2017-01-03 16:37:53 +0100

    Fix white space sql

--- install/sql/update.2.7.0.sql
+++ install/sql/update.2.7.0.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `mc_country` (
   PRIMARY KEY (`idcountry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-INSERT INTO `mc_module` (`id_module` ,`class_name` ,`name` ,`plugins`)VALUES (NULL , 'backend_controller_country', 'country', '0');
+INSERT INTO `mc_module` (`id_module` ,`class_name` ,`name` ,`plugins`) VALUES (NULL , 'backend_controller_country', 'country', '0');
 
 ALTER TABLE `mc_catalog` DROP `idadmin` ;
 
